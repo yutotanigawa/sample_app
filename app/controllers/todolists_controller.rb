90bab6766ba88a9925f599a4e5dc5aa0e7e41ade
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-07 15:54:15 +0900

    [fix]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,21 +1,24 @@
 class TodolistsController < ApplicationController
-  def new
-  	# Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
-  	@list = List.new
-  end
+
+    def new
+         @list = List.new
+    end
+
+    # 以下を追加
+    def create
+        # ストロングパラメーターを使用
+         list = List.new(list_params)
+        # DBへ保存する
+         list.save
+        # トップ画面へリダイレクト
+        redirect_to '/top'
+    end
+
+
+        private
+
+        def list_params
+        params.require(:list).permit(:title, :body)
+        end
+
 end
-
-def create
-	#ストロングパラメーターを使用
-	list = List.new(list_params)
-	# DBへ保存する
-	list.save
-	#トップ画面へリダイレクト
-	redirect_to '/top'
-end
-
-private
-
-	def list_params
-	params.require(:list).permit(:title,:body)
-	end
