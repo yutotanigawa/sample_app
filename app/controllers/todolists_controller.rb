4625ed13ef8f67a9ecfa3367263509423e508780
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-07 12:39:49 +0900

    [finish]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,21 @@
 class TodolistsController < ApplicationController
   def new
+  	# Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+  	@list = List.new
   end
 end
+
+def create
+	#ストロングパラメーターを使用
+	list = List.new(list_params)
+	# DBへ保存する
+	list.save
+	#トップ画面へリダイレクト
+	redirect_to '/top'
+end
+
+private
+
+	def list_params
+	params.require(:list).permit(:title,:body)
+	end
