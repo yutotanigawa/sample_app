1d6097f0982892443a1079e5b88d6763f68eda72
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-07 15:13:38 +0900

    [start]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   get 'todolists/new'
-
-get 'top' => 'homes#top'
+  get 'top' => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'todolists' => 'todolists#create'
+
+  get 'todolists' => 'todolists#index'
 end
